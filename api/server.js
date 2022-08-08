29afc3ccd2f94d6f99d5c477eb679f886593938d
Thanwar <60439635+Thanwar@users.noreply.github.com>
2022-08-08 20:09:50 +0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import mongoose from "mongoose";
+import cors from 'cors';
 
 const app = express()
 const port = 4000
+
+// app.use(bodyParser,urlencoded({extended:true}));
+// app.use(cookieParser())
+// cors()
+// app.use(cors({
+//     origin: 'localhost:3000',
+//     credentials: true,
+// }))
+
 
 app.get('/', (req, res) => {
   res.send('Hello Server!')
