ca0eb61b3479ad84aa107d3a0488228de3d2e616
Thanwar <60439635+Thanwar@users.noreply.github.com>
2022-08-09 00:19:00 +0500

    fixed signup

--- client/src/AuthModel.js
+++ client/src/AuthModel.js
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import Input from "./Input";
 import {useState} from 'react';
+import axios from 'axios'
 
 function AuthModel() {
     const [modalType, setModalType] = useState({initialState : 'login'});
@@ -13,12 +14,15 @@ function AuthModel() {
     function login()
     {
         console.log("login")
+        console.log(modalType.initialState)
+
     }
 
     function register (e) {
-        e.preventDefault();
-        const data = {email, username, password};
-        axios.post({url: 'http://localhost:4000/register'}, data, {config: {withCredentials:true}})        
+        // e.preventDefault();
+        // const data = {email, username, password};
+        // axios.post({url: 'http://localhost:4000/register'}, data, {config: {withCredentials:true}})        
+        console.log(modalType)
 
     }
 
@@ -28,7 +32,7 @@ function AuthModel() {
                 {modalType.initialState === 'login' && (
                     <h1 className="text-2xl mb-3" >Log In</h1>
                 )}
-                    {modalType.initialState === 'register' && (
+                {modalType.initialState === 'register' && (
                     <h1 className="text-2xl mb-3" >Sign Up</h1>
                 )}
                 {modalType.initialState === 'register' && (
@@ -45,12 +49,12 @@ function AuthModel() {
                 </Button>
                 {modalType.initialState === 'login' && (
                       <div>
-                        New to Reddit? <button onClick={() => setModalType({value: 'register'})} >SIGN UP</button>
+                        New to Reddit? <button onClick={() => setModalType({initialState: 'register'})} >SIGN UP</button>
                       </div>
                 )}
                    {modalType.initialState === 'register' && (
                       <div>
-                        Already have an account? <button onClick={() => setModalType({value: 'login'})} >Log In</button>
+                        Already have an account? <button onClick={() => setModalType({initialState: 'login'})} >Log In</button>
                       </div>
                 )}
               
