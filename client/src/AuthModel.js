8aa94a34538854823e5d6f29a781572d68d7deb6
Thanwar <60439635+Thanwar@users.noreply.github.com>
2022-08-08 23:53:58 +0500

    updated the mess

--- client/src/AuthModel.js
+++ client/src/AuthModel.js
@@ -8,33 +8,47 @@ function AuthModel() {
     const {username, setUsername} = useState({initialState: ''});
     const {password, setPassword} = useState({initialState: ''});
     
+    
+    //console.log(modalType.initialState)
+
+
+    function register()
+    {
+        console.log("register")
+    }
+
+    function login()
+    {
+        console.log("login")
+    }
 
     return(
         <div className="w-screen h-screen fixed top-0 left-0 z-20 flex" style={{backgroundColor:'rgba(0,0,0,.6)'}} >
             <div className=" border border-gray-700 w-3/4 sm:w-1/2 mx-auto bg-white p-5 self-center rounded-md" >
-                {modalType === 'login' && (
+                {modalType.initialState === 'login' && (
                     <h1 className="text-2xl mb-3" >Log In</h1>
                 )}
-                    {modalType === 'register' && (
+                    {modalType.initialState === 'register' && (
                     <h1 className="text-2xl mb-3" >Sign Up</h1>
                 )}
-                {modalType === 'register' && (
+                {modalType.initialState === 'register' && (
                     <Input type={"text"} className="placeholder:text-gray-500 placeholder:text-xs placeholder:font-bold mb-2 w-full " placeholder='EMAIL' value={email} onChange={e => setEmail(e.target.value)} />
 
                 )}
                 
                 
-                <Input type={"text"} className="placeholder:text-gray-500 placeholder:text-xs placeholder:font-bold mb-2 w-full " placeholder='USERNAME' value={username} onChange={e => setUsername(e.target.value)} />
-                <Input type={"password"} className="placeholder:text-gray-500 placeholder:text-xs placeholder:font-bold mb-2 w-full " placeholder='PASSWORD' value={password} onChange={e => setPassword(e.target.value)} /> 
-                <Button className=" w-full text-white" >
-                    {modalType === 'login' ? 'Log In' : 'Sign up' }
+
+                <Input type={"text"} className="placeholder:text-gray-500 placeholder:text-xs placeholder:font-bold mb-2 w-full " placeholder='USERNAME' />
+                <Input type={"password"} className="placeholder:text-gray-500 placeholder:text-xs placeholder:font-bold mb-2 w-full " placeholder='PASSWORD' />
+                <Button className=" w-full text-white" onClick={() => {modalType.initialState === 'login' ? login() : register() } }>
+                    {modalType.initialState === 'login' ? 'Log In' : 'Sign up' }
                 </Button>
-                {modalType === 'login' && (
+                {modalType.initialState === 'login' && (
                       <div>
                         New to Reddit? <button onClick={() => setModalType({value: 'register'})} >SIGN UP</button>
                       </div>
                 )}
-                   {modalType === 'register' && (
+                   {modalType.initialState === 'register' && (
                       <div>
                         Already have an account? <button onClick={() => setModalType({value: 'login'})} >Log In</button>
                       </div>
